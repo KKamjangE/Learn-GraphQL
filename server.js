e06ccc8d03dedb334a6b7634fae4b234a58668b6
KKamjangE <ajm980718@gmail.com>
2024-05-24 17:25:59 +0900

    Feat: apollo server 구성

--- server.js
+++ server.js
@@ -1,6 +1,65 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
 
-const server = new ApolloServer({})
+const tweets = [
+    {
+        id:'1',
+        text: 'first one',
+    },
+    {
+        id:'2',
+        text: 'second one',
+    }
+]
+
+const typeDefs = gql`
+    type User {
+        id: ID!
+        username: String!
+    }
+    type Tweet {
+        id: ID!
+        text: String!
+        author: User
+    }
+    type Query {
+        allTweet: [Tweet!]!
+        tweet(id: ID!): Tweet
+    }
+    type Mutation {
+        postTweet(text: String, userId: ID): Tweet
+        deleteTweet(id: ID): Boolean
+    }
+`
+
+const resolvers = {
+    Query: {
+        allTweet: () => {
+            return tweets
+        },
+        tweet: (root, { id }) => {
+            console.log(id);
+            return tweets.find((tweets) => tweets.id === id);
+        },
+    },
+    Mutation: {
+        postTweet: (_, { text, userId }) => {
+            const newTweet = {
+                id: tweets.length + 1,
+                text,
+            }
+            tweets.push(newTweet)
+            return newTweet
+        },
+        deleteTweet: (_, {id}) => {
+            const tweet = tweets.find((tweet) => tweet.id === id)
+            if (!tweet) return false;
+            tweets = tweets.filter((tweet) => tweet.id !== id)
+            return true;
+        }
+    }
+}
+
+const server = new ApolloServer({typeDefs, resolvers})
 
 server.listen().then(({url}) => {
     console.log(`Running on ${url}`);
